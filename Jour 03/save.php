89cd9b4eb261e4a7de90c159859dc4ad5506456e
Yanis Bennadij <yanis.bennadji@laplateforme.io>
2025-02-13 15:53:55 +0100

    all files updated so it WORKS

--- Jour 03/save.php
+++ Jour 03/save.php
@@ -6,14 +6,14 @@ $data = json_decode(file_get_contents('php://input'), true);
 
 if ($data) {
 // Charger les résultats existants
-$results = file_exists($file) ? json_decode(file_get_contents($file), true) : [];
-$results[] = $data;
+    $results = file_exists($file) ? json_decode(file_get_contents($file), true) : [];
+    $results[] = $data;
 
 // Sauvegarder les résultats
-file_put_contents($file, json_encode($results, JSON_PRETTY_PRINT));
-echo json_encode(["status" => "success"]);
+    file_put_contents($file, json_encode($results, JSON_PRETTY_PRINT));
+    echo json_encode(["status" => "success"]);
 } else {
-http_response_code(400);
-echo json_encode(["status" => "error", "message" => "Données invalides"]);
+    http_response_code(400);
+    echo json_encode(["status" => "error", "message" => "Données invalides"]);
 }
 ?>
